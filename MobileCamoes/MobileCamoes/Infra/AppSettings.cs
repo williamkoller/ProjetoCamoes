e6f71d8a348819e44f738cd6574d0a83fc6ba105
William Koller <wkoller25@gmail.com>
2020-04-14 20:26:18 -0300

    add key on project

--- MobileCamoes/MobileCamoes/Infra/AppSettings.cs
+++ MobileCamoes/MobileCamoes/Infra/AppSettings.cs
@@ -3,7 +3,7 @@
     public class AppSettings
     {
         public const string ApiUrl = "https://api.themoviedb.org/3";
-        public const string ApiKey = "";
+        public const string ApiKey = "a669ecde5c2ae8a2e13e8687d88a9ce3";
         public const string ApiImageBaseUrl = "https://image.tmdb.org/t/p/w500/";
     }
 }
